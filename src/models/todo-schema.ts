49a5ea6d9fe38bd5fcb5bacfd702cadf256af4f1
Jonatas Fiorentin <jonatasfiorentin@gmail.com>
2024-10-29 17:57:03 -0300

    fix: ajustando todo-schema

--- src/models/todo-schema.ts
+++ src/models/todo-schema.ts
@@ -6,7 +6,7 @@ export const todoSchema = z.object({
     .min(2, { message: 'O nome precisa ter pelo menos 2 caracteres' })
     .max(100, { message: 'O nome não pode exceder 100 caracteres' }),
   marcado: z.boolean().optional().default(false),
-  id: z.string().optional(),
+  id: z.string().nullish(),
 })
 
 export type Todo = z.infer<typeof todoSchema>
